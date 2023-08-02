49a97f41e86b5ef6439e34cc35dd7733ab5c7173
Laurent Lorena <laurent.lorena@gympass.com>
2023-08-02 11:00:53 -0300

    chore: basic struct and add navigation

--- src/Services/StackNavigation.ts
+++ src/Services/StackNavigation.ts
@@ -1,5 +1,3 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import WebView from './Webview';
@@ -7,15 +5,11 @@ import ListScreen from './ListScreen';
 
 const Stack = createNativeStackNavigator();
 
-function App(): JSX.Element {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
+const GitHubStackNavigation = () => {
+    return (
+        <Stack.Navigator>
         <Stack.Screen name="WebView" component={WebView} />
         <Stack.Screen name="ListScreen" component={ListScreen} />
       </Stack.Navigator>
-    </NavigationContainer>
-  );
+    )
 }
-
-export default App;
