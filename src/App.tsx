960e76f6523c56e8503a2afa4cd596c1d8f7fc74
Laurent Lorena <laurent.lorena@gympass.com>
2023-08-07 01:53:36 -0300

    chore: applying axios test and redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
 
 import GitHubStackNavigation from './Services/StackNavigation';
+import store from './Services/Redux/store';
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <GitHubStackNavigation />
-    </NavigationContainer>
+    <React.StrictMode>
+      <Provider store={store}>
+        <NavigationContainer>
+          <GitHubStackNavigation />
+        </NavigationContainer>
+      </Provider>
+    </React.StrictMode>
   );
 }
 
--- src/Services/StackNavigation.jsx
+++ src/Services/StackNavigation.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import WebView from '../Webview';
-import ListScreen from '../ListScreen';
+import WebView from '../Screens/Webview';
+import ListScreen from '../Screens/ListScreen';
 
 const Stack = createNativeStackNavigator();
 
